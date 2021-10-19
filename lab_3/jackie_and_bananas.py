cd5c3079caa2f94b0533ff1b16e2006d4e427c78
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-10-19 16:52:32 +0300

    Added task description to README file

--- lab_3/jackie_and_bananas.py
+++ lab_3/jackie_and_bananas.py
@@ -12,18 +12,18 @@ def count_eating_speed(piles, time):
         return 1
 
     min_eating_speed = ceil(all_bananas/time)
-    required_speed = min_eating_speed
+    k = min_eating_speed
 
     while True:
         time_for_pile = []
         required_time = 0
         for i in range(piles_num):
-            time_for_pile.append(ceil(piles[i]/required_speed))
+            time_for_pile.append(ceil(piles[i]/k))
             required_time += time_for_pile[i]
         if required_time <= time:
-            return required_speed
+            return k
         new_coefs = []
         for i in range(piles_num):
             if time_for_pile[i] > 1:
                 new_coefs.append(ceil(piles[i] / (time_for_pile[i] - 1)))
-        required_speed = min(new_coefs)
+        k = min(new_coefs)
